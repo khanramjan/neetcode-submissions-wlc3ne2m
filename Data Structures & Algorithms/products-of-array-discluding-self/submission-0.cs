488a037a0267b494872b80c9ec4bf779ebb159de
Ramjan Khan <108740692+khanramjan@users.noreply.github.com>
2026-04-07 17:57:28 +0600

    Add: products-of-array-discluding-self - submission-0

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-0.cs
@@ -0,0 +1,23 @@
+public class Solution {
+    public int[] ProductExceptSelf(int[] nums) {
+        int n=nums.Length;
+        int [] res= new int[n];
+        int [] s= new int[n];
+        int [] p= new int[n];
+        p[0]=1;
+        s[n-1]=1;
+        for(int i=1;i<n;i++)
+        {
+            p[i]=p[i-1]*nums[i-1];
+        }
+         for(int i=n-2;i>=0;i--)
+        {
+            s[i]=s[i+1]*nums[i+1];
+        }
+        for(int i=0;i<n;i++)
+        {
+            res[i]=s[i]*p[i];
+        }
+        return res;
+    }
+}
